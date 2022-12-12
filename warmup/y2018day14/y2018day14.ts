4dffab4562e34a093ff40beded5e4922a9f42906
Dan Vanderkam <danvdk@gmail.com>
2022-12-12 17:00:59 -0500

    y2018 day 14 star 2

--- warmup/y2018day14/y2018day14.ts
+++ warmup/y2018day14/y2018day14.ts
@@ -2,7 +2,7 @@
 // https://adventofcode.com/2018/day/14
 
 import { _ } from "../../deps.ts";
-import { safeParseInt } from "../../util.ts";
+import { assert, safeParseInt } from "../../util.ts";
 
 function part1(n: number): string {
   const recipes = [3, 7];
@@ -22,8 +22,32 @@ function part1(n: number): string {
   return recipes.slice(n, n+10).map(String).join('');
 }
 
+
+function part2(n: number): number {
+  const recipes = [3, 7];
+  const elves = [0, 1];
+  let step = 0;
+  const search = String(n).split('').map(Number);
+  const nDigits = search.length;
+  while (true) {
+    const newRecipes = _.sum(elves.map(i => recipes[i])).toString().split('').map(Number);
+    for (const r of newRecipes) {
+      recipes.push(r);
+    }
+    for (const [i, e] of elves.entries()) {
+      elves[i] = (e + 1 + recipes[e]) % recipes.length;
+    }
+    step++;
+    for (let i = recipes.length - nDigits - newRecipes.length; i < recipes.length - nDigits; i++) {
+      if (_.isEqual(recipes.slice(i, i + nDigits), search)) {
+        return i;
+      }
+    }
+  }
+}
+
 if (import.meta.main) {
   const [num] = Deno.args.map(safeParseInt);
   console.log('part 1', part1(num));
-  console.log('part 2');
+  console.log('part 2', part2(num));
 }
