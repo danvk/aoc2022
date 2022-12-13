39afa3b200430c1b69dc84735f7803f1ba79a6d7
Dan Vanderkam <danvdk@gmail.com>
2022-12-13 07:41:53 -0500

    day 13 star 2

--- day13/day13.ts
+++ day13/day13.ts
@@ -1,4 +1,4 @@
-#!/usr/bin/env -S deno run --allow-read --allow-write
+#!/usr/bin/env -S deno run --allow-read --allow-write --v8-flags=--stack-size=10000
 // https://adventofcode.com/2022/day/13
 
 import { _ } from "../deps.ts";
@@ -39,6 +39,18 @@ if (import.meta.main) {
       console.log(i, 'is ok');
     }
   }
+
+  const packets = [...pairs.flat().map(JSON.parse), [[2]], [[6]]];
+  packets.sort(compare);
+  console.log(packets);
+
+  for (let i = 0; i < packets.length; i++) {
+    const p = packets[i];
+    if (_.isEqual(p, [[2]]) || _.isEqual(p, [[6]])) {
+      console.log(i+1, p);
+    }
+  }
+
   console.log('part 1', sum);
   console.log('part 2');
 }
