773ec1af0738edbe2b14ffe13bf3b324604369bf
Dan Vanderkam <danvdk@gmail.com>
2022-12-20 07:07:11 -0500

    shifting circular list forward

--- day20/day20.ts
+++ day20/day20.ts
@@ -41,11 +41,49 @@ function printList(head: Node) {
   console.log(nums.map(String).join(', '));
 }
 
+function shift(n: Node, amount: number) {
+  let next = n.next;
+  for (let i = 0; i < amount; i++) {
+    next = next.next;
+  }
+
+  // 1, 2, -3, 3, -2, 0, 4
+  // A B C D
+
+  // next is n's new next.
+  const oldPrev = n.prev;  // A
+  const oldNext = n.next;  // C
+
+  //                    4      1            2        -3
+  // console.log(oldPrev.num, n.num, oldNext.num, next.num);
+
+  // remove n from the list
+  oldPrev.next = oldNext;  // A.next = C
+  oldNext.prev = oldPrev;  // C.prev = A
+
+  // add it back in the new spot
+  n.prev = next.prev;
+  n.next = next;           // D
+  next.prev = n;      // C
+  n.prev.next = n;
+}
+
 if (import.meta.main) {
   const lines = await readLinesFromArgs();
   const rawNums = lines.map(safeParseInt);
-  const nums = makeNodes(rawNums);
-  printList(nums[0]);
-  console.log('part 1', nums[0]);
-  console.log('part 2');
+  const nodes = makeNodes(rawNums);
+  printList(nodes[0]);
+  for (const node of nodes) {
+    console.log(node.num, 'prev=', node.prev.num, 'next=', node.next.num);
+  }
+  shift(nodes[0], nodes[0].num);
+
+  for (const node of nodes) {
+    console.log(node.num, 'prev=', node.prev.num, 'next=', node.next.num);
+  }
+  printList(nodes[0]);
+
+  // printList(nums[0]);
+  // console.log('part 1', nums[0]);
+  // console.log('part 2');
 }
