37673166c50472e9f47611fc347558f8e02ed4e5
Dan Vanderkam <danvdk@gmail.com>
2022-12-21 10:49:32 -0600

    y2018 day 23 star 1 I hope

--- warmup/y2018day23/y2018day23.ts
+++ warmup/y2018day23/y2018day23.ts
@@ -2,12 +2,30 @@
 // https://adventofcode.com/2018/day/23
 
 import { _ } from "../../deps.ts";
-import { readLinesFromArgs } from "../../util.ts";
+import { readInts, readLinesFromArgs } from "../../util.ts";
 
-start
+type Coord = [number, number, number];
+interface Nanobot {
+  pos: Coord;
+  r: number;
+}
+
+function parseNanobot(line: string): Nanobot {
+  const [a, b] = line.split(', ');
+  const pos = readInts(a, {expect: 3}) as Coord;
+  const [r] = readInts(b, {expect: 1});
+  return {pos, r};
+}
+
+function distance(a: Coord, b: Coord): number {
+  return Math.abs(a[0] - b[0]) + Math.abs(a[1] - b[1]) + Math.abs(a[2] - b[2]);
+}
 
 if (import.meta.main) {
   const lines = await readLinesFromArgs();
-  console.log('part 1', lines.length);
+  const bots = lines.map(parseNanobot);
+  const strongest = _.maxBy(bots, b => b.r)!;
+  const inRange = bots.filter(b => distance(b.pos, strongest.pos) <= strongest.r);
+  console.log('part 1', inRange.length);
   console.log('part 2');
 }
